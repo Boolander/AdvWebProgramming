162aae8e0b352eeebff95e3af5071c5c96a455d6
Christopher Booye <chris.booye@gmail.com>
2016-06-02 18:00:50 -0400

    Quiz 2 Stuff for Depot

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,66 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create!(title: 'CoffeeScript',
+Product.create!(title: 'Funko Pop - Tracer',
  description:
    %{<p>
-       CoffeeScript is JavaScript done right. It provides all of JavaScript's
-functionality wrapped in a cleaner, more succinct syntax. In the first
-book on this exciting new language, CoffeeScript guru Trevor Burnham
-shows you how to hold onto all the power and flexibility of JavaScript
-while writing clearer, cleaner, and safer code.
+       <em>From Overwatch, Tracer, as a stylized POP vinyl from Funko! Figure stands 
+       3 3/4 inches and comes in a window display box. Check out the other Overwatch 
+       figures from Funko! Collect them all!.
      </p>},
- image_url:   'cs.jpg',
- price: 36.00)
+ image_url: 'TracerPop.jpg',
+ price: 10.99)
 # . . .
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+Product.create!(title: 'Funko Pop - Winston',
  description:
    %{<p>
-       Ruby is the fastest growing and most exciting dynamic language
-       out there. If you need to get working programs delivered fast,
-       you should add Ruby to your toolbox.
+       <em>From Overwatch, Winston, as a stylized POP vinyl from Funko! Figure stands 
+       6 inches and comes in a window display box. Check out the other Overwatch 
+       figures from Funko! Collect them all!.
      </p>},
- image_url: 'ruby.jpg',
- price: 49.95)
+ image_url: 'WinstonPop.jpg',
+ price: 12.99)
 # . . .
-
-Product.create!(title: 'Rails Test Prescriptions',
+Product.create!(title: 'Funko Pop - Reaper',
  description:
    %{<p>
-       <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-       Rails applications, covering Test-Driven Development from both a
-       theoretical perspective (why to test) and from a practical perspective
-       (how to test effectively). It covers the core Rails testing tools and
-       procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-       including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+       <em>From Overwatch, Reaper, as a stylized POP vinyl from Funko! Figure stands 
+       3 3/4 inches and comes in a window display box. Check out the other Overwatch 
+       figures from Funko! Collect them all!.
      </p>},
- image_url: 'rtp.jpg',
- price: 34.95)
+ image_url: 'ReaperPop.jpg',
+ price: 10.99)
+# . . .
+Product.create!(title: 'Funko Pop - Widowmaker',
+ description:
+   %{<p>
+       <em>From Overwatch, Widowmaker, as a stylized POP vinyl from Funko! Figure stands 
+       3 3/4 inches and comes in a window display box. Check out the other Overwatch 
+       figures from Funko! Collect them all!.
+     </p>},
+ image_url: 'WidowmakerPop.jpg',
+ price: 10.99)
+ # . . .
+Product.create!(title: 'Overwatch - Origins Edition',
+ description:
+   %{<p>
+       <em>The future is now. Are you with us? Clash on the battlefields of tomorrow and choose your 
+       hero from a diverse cast of soldiers, scientists, adventurers, and oddities. Bend time, defy physics, 
+       and unleash an array of extraordinary powers and weapons. Engage your enemies in iconic locations from 
+       around the globe in the ultimate team-based shooter. Take your place in Overwatch. The world needs heroes. 
+     </p>},
+ image_url: 'OverwatchOriginsEdition.jpg',
+ price: 119.99)
+ # . . .
+Product.create!(title: 'Tracer Statue',
+ description:
+   %{<p>
+       <em>Celebrate the our newest, dynamic universe: OverwatchTM with this premium, 
+       polystone statue of TRACER! Sculpted by staff Senior Sculptor Brian Fay and painted 
+       by Senior Illustrator Laurel Austin, no detail has been lost in recreating Tracer's gravity 
+       (and time) defying action. Suspended mid stride, Tracer features prime, transparent energy 
+       effects and goggles, and is underlit by the illuminated OverwatchTM logo base! Measures 
+       10.5"/26.67cm (floor to head). Includes AC adapter and Battery option (batteries not included).
+     </p>},
+ image_url: 'TracerStatue.jpg',
+ price: 699.99)
